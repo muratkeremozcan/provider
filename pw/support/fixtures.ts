965e6dc6bee071a18441eb8fdb224c6694228218
muratkeremozcan <muratkerem@gmail.com>
2024-12-28 08:08:22 -0600

    30-test: pw-initial

---
+++ pw/support/fixtures.ts
@@ -0,0 +1,8 @@
+import { test as base, mergeTests } from '@playwright/test'
+import { test as apiRequestFixture } from './fixtures/api-request-fixture'
+// Merge the fixtures
+const test = mergeTests(apiRequestFixture) // add new fixtures as arguments
+
+const expect = base.expect
+
+export { test, expect }
